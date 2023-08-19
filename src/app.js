492443de0174846134906844f7b8d835ec1a1ae0
Andriian Sokolenko <asokolenko@BaDM.ua>
2023-08-19 22:28:31 +0300

    2

--- src/app.js
+++ src/app.js
@@ -7,13 +7,15 @@ import Header from "./components/header"
 import {observer} from "mobx-react-lite"
 import {algorithm} from "./services/theme-service"
 import Layout from "./components/layout";
+import Calculator from "./components/calculator"
 
 const App = observer(() => {
   return (
     <ConfigProvider locale={enUS} theme={algorithm()}>
       <Layout>
         <Header/>
-        <ContactsTable/>
+        <Calculator/>
+        {/*<ContactsTable/>*/}
       </Layout>
     </ConfigProvider>
   )
